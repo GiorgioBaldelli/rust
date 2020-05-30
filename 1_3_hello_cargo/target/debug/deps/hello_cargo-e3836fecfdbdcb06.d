1129f1d50b55b4daf5f86b8854f319e31308ef4f
Giorgio Baldelli <giorgio@formlabs.com>
2020-05-30 11:59:36 +0200

    hello cargo

---
+++ 1_3_hello_cargo/target/debug/deps/hello_cargo-e3836fecfdbdcb06.d
@@ -0,0 +1,5 @@
+/Users/giorgiobaldelli/Desktop/rust/hello_cargo/target/debug/deps/hello_cargo-e3836fecfdbdcb06: src/main.rs
+
+/Users/giorgiobaldelli/Desktop/rust/hello_cargo/target/debug/deps/hello_cargo-e3836fecfdbdcb06.d: src/main.rs
+
+src/main.rs:
